06e5271e3e9d00837aa89d0c33bbc137ae1e6786
Fernando Bevilacqua <dovyski@gmail.com>
2020-01-06 15:29:17 -0300

    Faz login de forma rudimentar no portal do professor

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ async function run() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    await page.goto('https://github.com');
-    await page.screenshot({ path: './github.png'});
+    await page.goto('https://professor.uffs.edu.br/');
+    await page.screenshot({ path: './professor.png'});
+
+    await page.type('[type=text][name=j_username]', '');
+    await page.type('[type=password][name=j_password]', '');
+    await page.click('[type=submit][value=Entrar]');
+
+    await page.waitForSelector('form[action$="inicio.xhtml"]');
+    await page.screenshot({ path: './professor2.png'});
 
     browser.close();
 }
