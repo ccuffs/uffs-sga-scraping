4b5809b7489b66bab0c883048c6c98c6e6920264
Fernando Bevilacqua <dovyski@gmail.com>
2021-01-16 18:27:21 -0300

    feat: obtenção de percentual de integralização

--- src/percentualIntegralizacao.js
+++ src/percentualIntegralizacao.js
@@ -1,14 +1,15 @@
 const path = require('path');
 
-async function baixaPlanilha(sga) {
+async function acessaPagina(sga) {
     const page = await sga.newTab();
     
-    await page.goto('https://professor.uffs.edu.br/restrito/graduacao/coordenador/percentual-integralizacao.xhtml');
-    await page.waitForSelector('a[title="Sair"]');
+    await page.goto('https://professor.uffs.edu.br/restrito/graduacao/coordenador/percentual-integralizacao.xhtml');    
+    await page.waitForSelector("a.ui-commandlink > img");
+    
+    return page;
+}
 
-    // TODO: pegar pasta de downloads do condig
-    await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: path.join(process.cwd(), 'data', 'downloads')});
-
+async function clicaBuscar(page) {
     const [button] = await page.$x("//a[img[contains(@title, 'Buscar')]]");
     
     if (button) {
@@ -24,6 +25,11 @@ async function baixaPlanilha(sga) {
     console.log('antes botao');
 
     await page.waitFor(2000);
+}
+
+async function baixaPlanilha(page) {
+    // TODO: pegar pasta de downloads do condig
+    await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: path.join(process.cwd(), 'data', 'downloads')});
 
     const [botaoGerarPlanilha] = await page.$x("//a[img[contains(@title, 'Gerar')]]");
     
@@ -36,11 +42,69 @@ async function baixaPlanilha(sga) {
     await page.waitFor(5000);
 }
 
-async function run(sga) {
-    // TODO: faz todas as ações do módulo
+async function extraiTabelaDaPagina(page) {
+    console.log('extraiTabelaDaPagina');
+
+    return await page.evaluate(() => {
+        var rows = [];
+        var columnNames = [];
+
+        // Obtem a tabela de conteudo
+        document.querySelectorAll('table[role="grid"]').forEach(function(table) {
+            console.log('header');
+
+            // Coleta o nome das colunas da tabela
+            table.querySelectorAll('thead tr[role="row"] th span').forEach(function(headerCell, idx) {
+                if(!headerCell.innerText) {
+                    return;
+                }
+
+                console.log(headerCell.innerText);
+                columnNames.push(headerCell.innerText);
+            });
+
+            console.log('rows');
+
+            // Itera em cada uma das linhas da tabela
+            table.querySelectorAll('tbody tr[role="row"]').forEach(function(row, idx) {
+                var entry = {};
+                var cells = row.getElementsByTagName('td');
+
+                if(cells.length == 0) {
+                    return;
+                }
+
+                for(var i = 0; i < cells.length; i++) {
+                    var columnName = columnNames[i];
+                    var columValue = cells[i].innerText;
+
+                    entry[columnName] = columValue;
+                }
+
+                rows.push(entry);
+            }); 
+        }); 
+
+        console.log(rows);
+        return rows;
+    });
+}
+
+async function run(sga, baixarPlanilha = false) {
+    const page = await acessaPagina(sga);
+
+    await clicaBuscar(page);
+
+    var integralizacoes = await extraiTabelaDaPagina(page);
+    
+    if(baixarPlanilha) {
+        await baixaPlanilha(page);
+    }
+
+    console.log(integralizacoes);
+    await page.waitFor(5000);
 }
 
 module.exports = {
-    run,
-    baixaPlanilha
+    run
 }
