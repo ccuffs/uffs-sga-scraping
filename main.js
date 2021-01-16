57640946ed14ac0359c68569e550d2cd76340305
Fernando Bevilacqua <dovyski@gmail.com>
2021-01-16 18:37:07 -0300

    feat: refatora organização geral para melhor organização

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-const config = require('./src/config');
+const config = require('./config.json');
+
 const sga = require('./src/sga');
 const percentualIntegralizacao = require('./src/percentualIntegralizacao');
 const historicoEscolar = require('./src/historicoEscolar');
 const listaAlunos = require('./src/listaAlunos');
 
 async function run() {
-    const instance = await sga.create(config.dev);
+    const instance = await sga.create(config);
     
-    await percentualIntegralizacao.baixaPlanilha(instance);
+    await percentualIntegralizacao.run(instance);
     await historicoEscolar.run(instance);
     await listaAlunos.run(instance);
 
